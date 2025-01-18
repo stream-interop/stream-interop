31c847ddded5c555dfdcce06954adb4f15c88eef
Paul M. Jones <pmjones@pmjones.io>
2025-01-18 10:11:15 -0600

    update Seekable to SeekableStream

---
+++ src/SeekableStream.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace StreamInterop\Interface;
+
+use RuntimeException;
+
+/**
+ * Contract for a seekable stream.
+ */
+interface SeekableStream
+{
+    /**
+     * Seek to a position within the stream.
+     *
+     * @throws RuntimeException on failure.
+     */
+    public function seek(int $offset, int $whence = SEEK_SET) : void;
+
+    /**
+     * Retrieve the current position of the buffer's internal pointer.
+     *
+     * @throws RuntimeException on failure.
+     */
+    public function tell() : int;
+
+    /**
+     * Rewind the stream back to the beginning.
+     *
+     * @throws RuntimeException on failure.
+     */
+    public function rewind() : void;
+}
