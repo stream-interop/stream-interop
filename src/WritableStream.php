176ede1f5546d893f193e5499335ea5d7bd213a4
Paul M. Jones <pmjones@pmjones.io>
2025-01-18 10:16:03 -0600

    update Writable to WritableStream

--- src/WritableStream.php
+++ src/WritableStream.php
@@ -3,25 +3,24 @@ declare(strict_types=1);
 
 namespace StreamInterop\Interface;
 
+use RuntimeException;
+use Stringable;
+
 /**
- * Contract for a write-only stream.
+ * Contract for a writable stream.
  *
- * A write-only stream is used when writing data to a "destination". For 
+ * A writeable stream is used when writing data to a "destination". For
  * example, writing to a console's ("stdout") or ("stderr") stream or when 
  * creating HTTP requests, on the client.
- *
- * @author Nathan Bishop (nbish11)
- * @copyright 2019 Nathan Bishop
- * @license MIT
  */
-interface Writable
+interface WritableStream extends Stream
 {
     /**
      * Write data to the stream.
      *
-     * @param string $data The content to be written to the stream.
-     * @throws \RuntimeException If the stream could not be written to. (E.g. stream was closed.)
+     * @param string|Stringable $data The content to be written to the stream.
+     * @throws RuntimeException If the stream could not be written to. (E.g. stream was closed.)
      * @return integer The number of bytes written to the stream.
      */
-    public function write(string $data): int;
+    public function write(string|Stringable $data) : int;
 }
