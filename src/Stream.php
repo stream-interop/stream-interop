6ca73112dd878fea749b02faff2e54e0c6f37ca4
Paul M. Jones <pmjones@pmjones.io>
2025-01-18 09:58:01 -0600

    add basic stream interface

---
+++ src/Stream.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace StreamInterop\Interface;
+
+/**
+ * @phpstan-type MetadataArray array{
+ *     timed_out?:bool,
+ *     blocked?:bool,
+ *     eof?:bool,
+ *     wrapper_data?:mixed[],
+ *     wrapper_type?:string,
+ *     stream_type:string,
+ *     mode:string,
+ *     unread_bytes:int,
+ *     seekable:bool,
+ *     uri?:string,
+ *     crypto?:mixed[]
+ * }
+ * 
+ * @see https://www.php.net/stream_get_meta_data
+ * @see php-src/ext/standard/streamsfuncs.c PHP_FUNCTION(stream_get_meta_data)
+ * @see php-src/ext/openssl/xp_ssl.c static int php_openssl_sockop_set_option
+ */
+interface Stream
+{
+    /**
+     * @var MetadataArray
+     */
+    public array $metadata { get; }
+
+    /**
+     * @var resource
+     */
+    public mixed $resource { get; }
+
+    public function getSize() : ?int;
+
+    public function isClosed() : bool;
+
+    public function isOpen() : bool;
+}
