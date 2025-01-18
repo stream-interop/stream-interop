3a6233b699723c63e0f6d5fb10beaf167a72b2e5
Paul M. Jones <pmjones@pmjones.io>
2025-01-18 10:06:02 -0600

    update Readable to ReadableStream

---
+++ src/ReadableStream.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace StreamInterop\Interface;
+
+use RuntimeException;
+
+/**
+ * Contract for readable streams.
+ *
+ * A readable stream is used when reading data from a "source". For example, reading
+ * input from the console ("stdin").
+ */
+interface ReadableStream extends Stream
+{
+    /**
+     * Retrieve content (in bytes) from the source.
+     *
+     * @param int<1,max> $length The number of bytes to retrieve.
+     * @throws RuntimeException on failure.
+     */
+    public function read(int $length) : string;
+
+    /**
+     * Copy the contents of this stream to another.
+     *
+     * @param Writable $destination The stream to write to.
+     * @return Writable The stream being written to.
+     */
+    public function pipe(Writable $destination) : Writable;
+
+    /**
+     * Determines if the stream has read to end-of-file.
+     *
+     * @return boolean
+     */
+    public function eof() : bool;
+
+    /**
+     * Retrieve the remaining contents in the buffer.
+     *
+     * @throws RuntimeException on failure.
+     */
+    public function getContents() : string;
+}
