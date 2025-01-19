bc4f0dd3bfe7a7366a2af9b3b0a5b216b6259cd5
Paul M. Jones <pmjones@pmjones.io>
2025-01-19 16:04:18 -0600

    consider a separate SizableInterface

---
+++ src/SizableStream.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace StreamInterop\Interface;
+
+interface SizableStream extends Stream
+{
+    /**
+     * @return int<0, max>
+     */
+    public function getSize() : int;
+}
